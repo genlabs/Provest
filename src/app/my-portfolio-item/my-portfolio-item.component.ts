debff7b51c2db0406f279d918c45f47051bfaa20
jyew1992 <jyew1992@gmail.com>
2016-11-12 17:41:44 +0800

    add two images

--- src/app/my-portfolio-item/my-portfolio-item.component.ts
+++ src/app/my-portfolio-item/my-portfolio-item.component.ts
@@ -3,19 +3,19 @@ import {CurrentUserService} from '../current-user.service';
 import {MyPortfolio} from '../my-portfolio';
 
 @Component({
-  selector: 'app-my-portfolio-item',
+  selector: 'my-portfolio-item',
   templateUrl: './my-portfolio-item.component.html',
   styleUrls: ['./my-portfolio-item.component.css']
 })
 export class MyPortfolioItemComponent implements OnInit {
 
 	private _myPortfolio:MyPortfolio[]= [];
+
   constructor(private userService:CurrentUserService) { }
 
   ngOnInit() {
 
-  	     // this._myPortfolio = 
-  	     console.log(this.userService.getCurrentUser());
+  	     this._myPortfolio = this.userService.getCurrentUser().myPortfolio;
   }
 
   @Input()
