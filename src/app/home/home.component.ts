ae0fddb047dc80eb49c28187bbfff023583dc5e6
jyew1992 <jyew1992@gmail.com>
2016-11-12 14:17:40 +0800

    Revert "Revert "added carousel""
    This reverts commit 5762bfff41e5932d265e57da0356ef46f0cc0057.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -13,3 +14,4 @@ export class HomeComponent implements OnInit {
   }
 
 }
+
