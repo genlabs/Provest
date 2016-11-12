61c29b4406b17f247ffb2a25a3f4b82e3bceb48c
jyew1992 <jyew1992@gmail.com>
2016-11-12 21:55:42 +0800

    pie chart adjusted

--- src/app/my-portfolio-item/my-portfolio-item.component.ts
+++ src/app/my-portfolio-item/my-portfolio-item.component.ts
@@ -12,6 +12,7 @@ export class MyPortfolioItemComponent implements OnInit {
 	private _myPortfolio:MyPortfolio[]= [];
   private totalInvestmentAmount = 0;
   private totalMoneyEarnedFromEachPlan:number[] = [];
+  private totalMoneyInvestedForEachPlan:number[] = [];
   private totalCollectedAmount= 0;
   private propertyName:string[] = [];
 
@@ -21,6 +22,7 @@ export class MyPortfolioItemComponent implements OnInit {
 
   	     this._myPortfolio = this.userService.getCurrentUser().myPortfolio;
          this.totalMoneyEarnedFromEachPlan = [];
+         this.totalMoneyInvestedForEachPlan = [];
          this.propertyName = [];
          var moneyEarned = 0;
 
@@ -30,6 +32,7 @@ export class MyPortfolioItemComponent implements OnInit {
             this.totalMoneyEarnedFromEachPlan.push(moneyEarned);
             this.totalCollectedAmount += moneyEarned;
             this.propertyName.push(this._myPortfolio[i].property.name);
+            this.totalMoneyInvestedForEachPlan.push(this._myPortfolio[i].investedAmount);
          }
 
   }
