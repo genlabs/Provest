1274546e8acf88847341841e4775a531eee1c33c
Sunny Cheung <sunnycyk@yahoo.com>
2016-11-13 10:14:33 +0800

    fix typescript bug

--- src/app/mortgage-resolve.service.ts
+++ src/app/mortgage-resolve.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 
 import { Mortgage } from './mortgage';
-import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, ActivatedRoute } from '@angular/router';
 import { MortgagesService  } from './mortgages.service';
 
 
 @Injectable()
 export class MortgageResolveService implements Resolve<Mortgage> {
 
-  constructor(private mortgageService:MortgagesService) { }
+  constructor(private mortgageService:MortgagesService, private route:ActivatedRoute) { }
 
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ) {
-    return this.mortgageService.getMortgageById(route.params.mId);
+  ){
+    
+    return this.mortgageService.getMortgageById(route.params["mId"]);
   }
 
 }
